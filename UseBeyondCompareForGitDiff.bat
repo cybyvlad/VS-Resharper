2dd571e214eaa0b615431787f4122d30fd3f7194
cybyvlad <cybyvlad@gmail.com>
2012-07-12 14:37:44 +0300

    Added beyond compare for git config

---
+++ UseBeyondCompareForGitDiff.bat
@@ -0,0 +1,4 @@
+ git config --global diff.tool bc3
+ git config --global difftool.bc3.path "c:\Program Files (x86)\Beyond Compare 3\BComp.exe"
+ git config --global merge.tool bc3
+ git config --global mergetool.bc3.path "c:\Program Files (x86)\Beyond Compare 3\BComp.exe"
